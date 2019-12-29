02df7f1fac3d7f8142553d2017069ea96c7696df
Michal Adamski <mj_Aadamski@wp.pl>
2019-12-29 13:24:18 +0100

    update few html issues with generating links

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 
     path('about/', views.about, name='about'),
-    path('post/', views.post, name='post'),
+#    path('post/', views.post, name='post'),
     path('contact/', views.contact, name='contact'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,7 +6,7 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').asc()
     return render(request, 'blog/post_list.html', {})
 
 def index(request):
@@ -53,6 +53,11 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
 
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
